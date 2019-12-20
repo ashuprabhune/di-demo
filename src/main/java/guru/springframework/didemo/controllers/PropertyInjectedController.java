79986c68c60885bd3491173597495ec5d1be06a2
Ashish Prabhune <ashuprabhune@gmail.com>
2019-12-19 20:06:35 -0500

    Added Primary Service

--- src/main/java/guru/springframework/didemo/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/didemo/controllers/PropertyInjectedController.java
@@ -3,15 +3,17 @@ package guru.springframework.didemo.controllers;
 import guru.springframework.didemo.services.GreetingService;
 import guru.springframework.didemo.services.GreetingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
     @Autowired
-    public GreetingServiceImpl greetingService;
+    @Qualifier("greetingServiceImpl")
+    public GreetingService greetingServiceImpl;
 
     public String sayHello()
     {
-        return  greetingService.sayGreeting();
+        return  greetingServiceImpl.sayGreeting();
     }
 }
